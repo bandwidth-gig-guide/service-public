e0abf2e5acb77dabbc16e9d3387d21f7f57898f8
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 14:55:56 +1000

    feat: filter events by venue location

--- app/api/event.py
+++ app/api/event.py
@@ -33,7 +33,8 @@ def get_complete_(event_id: UUID):
 @event.get("/", response_model=list[UUID])
 def get_all_id_(
     name: Optional[str] = None,
-    city: Optional[str] = None,
+    stateCode: Optional[str] = None,
+    city: Optional[list[str]] = Query(default=None),
     maxPrice: Optional[int] = None,
     types: Optional[list[str]] = Query(default=None),
     tags: Optional[list[str]] = Query(default=None),
@@ -41,6 +42,7 @@ def get_all_id_(
 ):
     return get_all_id(
         name=name,
+        stateCode=stateCode,
         city=city,
         maxPrice=maxPrice,
         types=types,
