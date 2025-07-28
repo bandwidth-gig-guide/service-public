99668c15ccf87fb411a3b4b71c299132da910774
MatthewLeigh <matthewcross97@gmail.com>
2025-07-28 14:46:19 +1000

    feat: update query to accept params

--- app/rest/venue/get_all_id.py
+++ app/rest/venue/get_all_id.py
@@ -1,10 +1,79 @@
-from app.query.fetch_list import execute
 from uuid import UUID
+from typing import Optional, List, Tuple
+from app.query.fetch_list import execute
+from app.util.list_to_array_string import list_to_array_string
 
-def get_all_id() -> list[UUID]:
-    rows = execute(query())
+
+def get_all_id(
+    name: Optional[str] = None,
+    stateCode: Optional[str] = None,
+    city: Optional[List[str]] = None,
+    types: Optional[List[str]] = None,
+    tags: Optional[List[str]] = None,
+) -> list[UUID]:
+    
+    query, params = prepare(
+        name=name,
+        stateCode=stateCode,
+        city=city,
+        types=types,
+        tags=tags
+    )
+    rows = execute(query, params)
     return [row[0] for row in rows]
 
 
-def query():
-    return "SELECT VenueID FROM Venue"
+def prepare(
+    name: Optional[str] = None,
+    stateCode: Optional[str] = None,
+    city: Optional[List[str]] = None,
+    types: Optional[List[str]] = None,
+    tags: Optional[List[str]] = None,
+) -> Tuple[str, list]:
+    
+    query = "SELECT VenueID FROM Venue"
+
+    filters = []
+    params = []
+
+    if name:
+        filters.append("Venue.Title ILIKE %s")
+        params.append(f"%{name}%")
+
+    if stateCode:
+        filters.append("Venue.StateCode = %s")
+        params.append(stateCode)
+
+    if city:
+        filters.append("""
+            EXISTS (
+                SELECT 1 FROM Venue
+                WHERE Venue.City = ANY(%s)
+            )
+        """)
+        params.append(list_to_array_string(city))
+
+    if types:
+        filters.append("""
+            EXISTS (
+                SELECT 1 FROM VenueType
+                WHERE VenueType.VenueID = Venue.VenueID
+                AND VenueType.Type = ANY(%s::TYPEVENUE[])
+            )
+        """)
+        params.append(list_to_array_string(types))
+
+    if tags:
+        filters.append("""
+            EXISTS (
+                SELECT 1 FROM VenueTag
+                WHERE VenueTag.VenueID = Venue.VenueID
+                AND VenueTag.Tag = ANY(%s::TAG[])
+            )
+        """)
+        params.append(list_to_array_string(tags))
+
+    if filters:
+        query += " WHERE " + " AND ".join(filters)
+
+    return query, params
