e9c9d7060c43bc2d160903701df82d51d52f07a3
MatthewLeigh <matthewcross97@gmail.com>
2025-09-16 23:17:43 +1000

    feat: add function to retrieve distinct cities from Venue

---
+++ app/rest/venue/get_cities.py
@@ -0,0 +1,9 @@
+from app.query.fetch_list import execute
+
+def get_cities() -> list[str]:
+    rows = execute(query)
+    return [row[0] for row in rows]
+
+query = "SELECT DISTINCT City FROM Venue;"
+
+
