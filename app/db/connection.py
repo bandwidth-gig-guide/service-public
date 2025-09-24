45e6f90c362498c4fc98fc01cafc58365f76d166
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-24 13:42:19 +1000

    feat: log db config and success

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,12 +1,13 @@
 from fastapi import HTTPException
-from dotenv import load_dotenv
 import psycopg2
-
 from app.config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
-load_dotenv()
-
 def get_db_connection():
+    print("INFO:     Attempting DB connection")
+    print(f"INFO:     host: {DB_HOST}")
+    print(f"INFO:     user: {DB_USER}")
+    print(f"INFO:     password: {DB_PASSWORD[:3] + '*' * (len(DB_PASSWORD) - 3) if DB_PASSWORD and len(DB_PASSWORD) > 3 else DB_PASSWORD}")
+    print(f"INFO:     dbname: {DB_NAME}")
     try:
         connection = psycopg2.connect(
             host=DB_HOST,
@@ -14,6 +15,9 @@ def get_db_connection():
             password=DB_PASSWORD,
             dbname=DB_NAME
         )
+        print("INFO:     DB connection successful!")
         return connection
+    
     except psycopg2.Error as e:
+        print(f"ERROR:    DB connection failed: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error connecting to database: {str(e)}")
