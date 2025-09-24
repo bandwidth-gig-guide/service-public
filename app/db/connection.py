290d4316ac3104538588ce4ff267d37e5993657e
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-24 14:05:00 +1000

    fix: allow port to be set for connection
    postgres default is 5432, but digital ocean typically runs postgres managed db's on 25060.

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,14 +1,16 @@
 from fastapi import HTTPException
 import psycopg2
-from app.config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+from app.config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
 
 def get_db_connection():
     try:
         connection = psycopg2.connect(
             host=DB_HOST,
+            port=DB_PORT,
             user=DB_USER,
             password=DB_PASSWORD,
-            dbname=DB_NAME
+            dbname=DB_NAME,
+            sslmode="require"
         )
         return connection
     
