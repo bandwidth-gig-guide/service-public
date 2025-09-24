cf3ac34643d47650a23495239789cb7d79a3a56b
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-09-24 14:08:36 +1000

    fix: update env variable that is used for port number
    forgot to change that part when i copied the line above.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 DB_HOST = os.getenv('DB_HOST', 'db-main')
-DB_PORT = os.getenv('DB_HOST', '5432')
+DB_PORT = os.getenv('DB_PORT', '5432')
 DB_USER = os.getenv('DB_USER', 'root')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
 DB_NAME = os.getenv('DB_NAME', 'db-main')
