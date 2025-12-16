4988ead59bb5c2dbe9114d3f12aeef7a28e7d40b
Matthew Cross <119134131+MatthewLeigh@users.noreply.github.com>
2025-12-16 18:16:46 +1100

    feat: implement caching for artist API endpoints

--- app/api/artist.py
+++ app/api/artist.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter, Query
 
 from typing import Optional
 from uuid import UUID
+from app.cache.redis import make_key, cache_wrap
+from app.cache.key import ARTIST
 from app.model.artist_brief import ArtistBrief
 from app.model.artist import Artist
+
 
 from app.rest.artist.get_brief import get_brief
 from app.rest.artist.get_all_brief import get_all_brief
@@ -14,25 +17,34 @@ from app.rest.artist.get_recommended import get_recommended
 
 artist = APIRouter()
 
+
 # GET Single brief
 @artist.get("/brief/{artist_id}", response_model=ArtistBrief)
 def get_brief_(artist_id: UUID):
-    return get_brief(artist_id)
+    key = make_key(ARTIST.BRIEF, artist_id=str(artist_id))
+    return cache_wrap(key, lambda: get_brief(artist_id))
+
 
 # GET All briefs
 @artist.get("/brief", response_model=list[ArtistBrief])
 def get_all_brief_():
-    return get_all_brief()
+    key = make_key(ARTIST.BRIEF)
+    return cache_wrap(key, lambda: get_all_brief())
+
 
 # GET Recommended
 @artist.get("/recommended/{artist_id}", response_model=list[UUID])
 def get_recommended_(artist_id: UUID):
-    return get_recommended(artist_id)
+    key = make_key(ARTIST.RECOMMENDED, artist_id=str(artist_id))
+    return cache_wrap(key, lambda: get_recommended(artist_id))
+
 
 # GET Single Complete
 @artist.get("/{artist_id}", response_model=Artist)
 def get_complete_(artist_id: UUID):
-    return get_complete(artist_id)
+    key = make_key(ARTIST.DETAILED, artist_id=str(artist_id))
+    return cache_wrap(key, lambda: get_complete(artist_id))
+
 
 # GET IDs | Filterable
 @artist.get("/", response_model=list[UUID])
@@ -45,12 +57,14 @@ def get_all_id_(
     tags: Optional[list[str]] = Query(default=None),
     hasUpcomingEvent: Optional[bool] = None,
 ):
-    return get_all_id(
-        name=name,
-        country=country,
-        city=city,
-        stateCodes=stateCodes,
-        types=types,
-        tags=tags,
-        hasUpcomingEvent=hasUpcomingEvent
-    )
+    filters = {
+        "name": name,
+        "country": country,
+        "city": city,
+        "stateCodes": stateCodes,
+        "types": types,
+        "tags": tags,
+        "hasUpcomingEvent": hasUpcomingEvent,
+    }
+    key = make_key(ARTIST.IDS, **filters)
+    return cache_wrap(key, lambda: get_all_id(**filters))
